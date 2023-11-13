26904ea0ec7f65769fd3e289cf26a77d9508d517
Olugbade Ayodeji <olugbade.ayodeji@gmail.com>
2023-11-13 03:00:53 +0100

    added post method to the form in signup file

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
+const axios = require('axios');     
 
 
 const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/signup.html");
-})
+
+    let firtsName = req.body.fName;
+    let lastName = req.params.lName;
+    let email = req.body.email;
+
+    console.log(firtsName, lastName, email);
+});
 
 app.listen(3000, function () {
     console.log('server is running on port 3000');
-})
+});
