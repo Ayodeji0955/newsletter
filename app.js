131737a3f4f929ad293e72c3d20ad48d2e352a6e
Olugbade Ayodeji <olugbade.ayodeji@gmail.com>
2023-11-14 16:03:56 +0100

    new commit

--- app.js
+++ app.js
@@ -11,12 +11,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/signup.html");
 
-    let firtsName = req.body.fName;
-    let lastName = req.params.lName;
-    let email = req.body.email;
-
-    console.log(firtsName, lastName, email);
 });
+
+app.post("/", function(req, res){
+    
+    let firstName = req.body.fName;
+    let lastName = req.body.lName;
+    let email = req.body.email;
+    
+    console.log(firstName, lastName, email);
+})
 
 app.listen(3000, function () {
     console.log('server is running on port 3000');
