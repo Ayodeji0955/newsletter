3b3d878aef32308e1a04e855c81cc5dc51fc916f
Olugbade Ayodeji <olugbade.ayodeji@gmail.com>
2023-11-22 13:51:05 +0100

    new commit

--- app.js
+++ app.js
@@ -9,10 +9,22 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/signup.html");
+// app.get("/", function(req, res) {
+//     res.sendFile(__dirname + "/signup.html");
 
-});
+// });
+
+
+app.get('/', (req, res) => {
+    axios.get(__dirname + '/signup.html')
+      .then(response => {
+        res.send(response.data);
+      })
+      .catch(error => {
+        res.status(500).send('An error occurred');
+      });
+  });
+
 
 app.post("/", function(req, res){
     
@@ -23,6 +35,9 @@ app.post("/", function(req, res){
     console.log(firstName, lastName, email);
 })
 
-app.listen(3000, function () {
+app.listen(3000, function() {
     console.log('server is running on port 3000');
 });
+
+
+// 868d0cf8860880853b234e9e273d01d2-us14
